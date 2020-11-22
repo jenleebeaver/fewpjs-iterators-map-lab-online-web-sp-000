65a95fc0753c1619a6ce02b5bf2c6ffda7708b7f
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-11-22 16:42:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,12 +21,4 @@ function titleCased() {
   });
 }
 
-function titleCased() {
-  return tutorials.map(function (tutorial) {
-    tutorial = tutorial.split(' ');
-    for (var i = 0; i < tutorial.length; i++) {
-      tutorial[i] = tutorial[i].charAt(0).toUpperCase() + tutorial[i].slice(1);
-    }
-    return tutorial.join(' ');
-  });
-}
+//.slice is used to return specific elements 
